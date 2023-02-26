a935f131313be17ea997508593d2208b1c1ece1e
Luis Gonzalez <investlagh@hotmail.com>
2023-02-26 18:10:50 -0500

    Added the Ajax calls

--- sextant/src/Exhibition.js
+++ sextant/src/Exhibition.js
@@ -1,11 +1,29 @@
 import CardDisplay from './Card.js';
+import {useEffect, useState, useRef} from 'react';
+
+
 
 function Exhibition() {
+    const [Api4, setApi4] = useState([]);
+    const [Api6, setApi6] = useState([]);
+
+    useEffect(()=>{
+        fetch('https://api64.ipify.org?format=json')
+          .then(response => response.json())
+          .then(setApi6);
+       }, []);
+
+       useEffect(()=>{
+        fetch('https://api.ipify.org?format=json')
+          .then(response => response.json())
+          .then(setApi4);
+       }, []);
+
     return (
         <div className='Exhibition-deck'>
-                <CardDisplay name="Title #1" description="Description #1" />
-                <CardDisplay name="Title #2" description="Description #2" />
-                <CardDisplay name="Title #3" description="Description #3" />
+                <CardDisplay name="IPV4" description={Api4.ip} />
+                <CardDisplay name="IPV6" description={Api6.ip} />
+                
         </div>
     );
 }
